da3d3eb6fa7668085a9570123f528b2dc724eea3
Юрий Райков <Therion988@yandex.ru>
2022-04-24 11:23:31 +0300

    Добавление try для безопасной раоты сервера с потоками

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,7 +11,7 @@ public class Main {
         System.out.println("server started");
         int port = 8089;
         ServerSocket serverSocket = new ServerSocket(port);
-         while(true){
+         try {
              Socket clientSocket = serverSocket.accept();
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -20,6 +21,8 @@ public class Main {
              final String name = in.readLine();
              out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
 
-         }
+         } catch (IOException e) {
+        System.err.println(e);
+    }
     }
 }
